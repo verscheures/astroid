c148adfb6ea87683962b531150ead0f83970b65b
Stefaan Verscheure <verscheure.stefaan@gmail.com>
2024-12-03 23:22:19 +0100

    EOD

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,13 +13,31 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    astroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, astroids)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    astroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+
+        for drawable_object in drawable:
+                drawable_object.draw(screen)
+        for updatable_object in updatable:
+            updatable_object.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
